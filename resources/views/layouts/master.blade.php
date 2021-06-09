cbee89382d47de223d82cb72658169b7e2c9b5d3
Dinmukhamed Tezekbayev <din.tezekbayev@gmail.com>
2021-06-09 22:25:44 +0600

    убрали тест

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -63,7 +63,7 @@
         <a href="{{ route('home') }}" class="brand-link">
 {{--            <img src="{{  Config::get('settings.logo') }}" alt="{{  Config::get('settings.name') }}" class="brand-image img-circle elevation-3"--}}
 {{--                 style="opacity: .8">--}}
-            <span class="brand-text font-weight-light">{{  Config::get('settings.name') }} <span class="right badge badge-danger">тест</span></span>
+            <span class="brand-text font-weight-light">{{  Config::get('settings.name') }}</span>
         </a>
 
         <!-- Sidebar -->
